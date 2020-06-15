6fe382f9dfd5b198bd9802c34fa140fcc46a0a14
SSGean <gean.sousa.silva@gmail.com>
2020-06-15 01:57:57 -0300

    Comparativo JS

---
+++ Curso JavaScript/Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade do carro é de ${vel}Km/h`)
+if ( vel > 60) {
+    console.log('Você ultrapassou a velocidade da via. MULTADO!')
+}
+console.log('Dirija com cuidado!')
+
