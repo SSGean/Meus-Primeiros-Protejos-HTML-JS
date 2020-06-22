8229fe2755b0db628eed2500c8597842d9f51638
SSGean <gean.sousa.silva@gmail.com>
2020-06-22 20:39:35 -0300

    Exercício verificador de idade com imagens

--- Curso JavaScript/Ex_data_e_hora/ex015/script.js
+++ Curso JavaScript/Ex_data_e_hora/ex015/script.js
@@ -3,17 +3,46 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.lenght == 0 || fano.value > ano) {
-        window.alert('Verifique os dados e tente novamente!')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
-            gênero = 'Masculino'
+            gênero = 'masculino'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'homem-crianca.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'homem-jovem.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'homem-adulto.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'homem-idoso.png')
+            }
         } else if (fsex[1].checked) {
-            gênero = 'Feminino'
+            gênero = 'feminino'
+            if (idade >= 0 && idade <10) {
+                //Criança
+                img.setAttribute('src', 'mulher-crianca.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'mulher-jovem.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulher-adulta.png')
+            } else {
+                //Idosa
+                img.setAttribute('src', 'mulher-idosa.png')
+            }
         }
         res.innerHTML = `Detectada uma pessoa do sexo ${gênero} com ${idade} anos!`
+        res.appendChild(img)
     }
 }
