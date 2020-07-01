6f711b0f0f33a55181f5001da3098f32b32ad497
SSGean <gean.sousa.silva@gmail.com>
2020-07-01 14:11:55 -0300

    Desafio calculadora de desconto

---
+++ Curso JavaScript/Exercicios de fixação solo/Desafios Curso em Video/8 - Desconto/script.js
@@ -0,0 +1,28 @@
+function converter() {
+    
+    var produto = window.prompt('Qual é o produto que você está comprando?')
+
+    var preco = Number(window.prompt(`Qual é o preço de ${produto}`).replace(',' , '.'));
+    
+    var desc = 10 * (preco / 100) 
+
+    var preco_desc = preco - desc
+ 
+
+    document.getElementById('div#res');
+
+    preco = preco.toLocaleString('pt-BR' , {style:'currency' , currency:'BRL'})
+
+    desc = desc.toLocaleString('pt-BR' , {style:'currency' , currency:'BRL'})
+
+    preco_desc = preco_desc.toLocaleString('pt-BR' , {style:'currency' , currency:'BRL'})
+    
+    res.innerHTML = (` <b> Calculando desconto de 10% para ${produto}...</b><br>
+    <br>
+    O preço original era de ${preco}. <br>
+    <br>
+    Você acaba de ganhar ${desc} de desconto (-10). <br>
+    <br>
+    No fim, você vai pagar ${preco_desc}.`);
+
+}
