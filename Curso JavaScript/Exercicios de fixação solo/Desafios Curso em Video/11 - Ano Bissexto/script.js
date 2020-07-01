f02a75bec7f9f36349b27b5b3dcacd8acd4bf545
SSGean <gean.sousa.silva@gmail.com>
2020-07-01 18:01:56 -0300

    Desafio Ano Bissexto
    Inclusão de novo exercício e renumeração de pastas para melhor organização

---
+++ Curso JavaScript/Exercicios de fixação solo/Desafios Curso em Video/11 - Ano Bissexto/script.js
@@ -0,0 +1,19 @@
+function verificar() {
+
+    var ano = Number(window.prompt('Qual é o ano que você quer verificar?'));
+
+    var bissexto = (ano % 4);
+
+    document.getElementById('div#res');
+
+    if (bissexto == 0) {
+        res.innerHTML = (`<h3>Analisando o ano de ${ano}...</h3>
+    <p class="p1">O ano de ${ano} <b><span class="green"> É BISSEXTO</span>&#9989</b></p>`);
+    } else {
+        res.innerHTML = (`<h3>Analisando o ano de ${ano}...</h3>
+      <p class="p2">O ano de ${ano} <b><span class="red">NÃO É BISSEXTO</span> &#10060</b></p>`);
+    }
+
+
+
+}
